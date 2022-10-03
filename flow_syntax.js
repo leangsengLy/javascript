271708a8536f8e20645dd7783c4c04a133f5b342
leangsengLy <edeepleang@gmail.com>
2022-10-03 16:40:30 +0700

    object

--- flow_syntax.js
+++ flow_syntax.js
@@ -1,9 +1,9 @@
+
+
 function saysomething(){
     console.log("hello world to Javascript ! ");
 }
-saysomething();
-saysomething();
-saysomething();
+
 
 let sayhi = function(){
     console.log("Let me say hi to you ! ");
@@ -16,7 +16,39 @@ const fun = function(){
 }
 fun();
 
-function changepercent(){
-    document.getElementById('changepercent').innerHTML = "hELLO MR LEANGSENG";
+// object
+let namez = {
+    name1 : "hong",
+    name2 : "leangseng",
+    name3 : 'Kunitha',
+    age: 20
 }
-changepercent();
+
+function changepercent(name,name1){
+    document.getElementById('changepercent').innerHTML = name+"ANd "+ name1;
+}
+changepercent(namez.name3);
+
+
+// method in object
+ let person = {
+      firstname : 'ly',
+      lastname: 'leangseng',
+      age:20,
+      showIformation: function(){
+        changepercent(this.lastname);
+      }
+ }
+person.showIformation();
+
+
+
+// pass object to function
+// create function
+// change age 
+let changeage= function(Obj){
+    Obj.age = 100;
+    Obj.firstname = 'Ah ja leang';
+}
+changeage(person);
+changepercent(person.age,person.firstname);
