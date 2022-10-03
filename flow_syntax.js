01bc5daae952e10b120809a9fddbc12ab747b6e3
leangsengLy <edeepleang@gmail.com>
2022-10-03 16:10:02 +0700

    day1

---
+++ flow_syntax.js
@@ -0,0 +1,22 @@
+function saysomething(){
+    console.log("hello world to Javascript ! ");
+}
+saysomething();
+saysomething();
+saysomething();
+
+let sayhi = function(){
+    console.log("Let me say hi to you ! ");
+}
+sayhi();
+
+//function with parameter
+const fun = function(){
+    console.log("hello");
+}
+fun();
+
+function changepercent(){
+    document.getElementById('changepercent').innerHTML = "hELLO MR LEANGSENG";
+}
+changepercent();
